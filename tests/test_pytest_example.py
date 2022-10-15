5e859bff4bbaf0bf3310f3d95390383cbb578c03
umt0120 <uemoto0120@gmail.com>
2022-10-15 22:39:05 +0900

    refactor: lint on black

--- tests/test_pytest_example.py
+++ tests/test_pytest_example.py
@@ -1,13 +1,15 @@
 from pathlib import Path
-from pytest_example.pytest_example import getssh
+
 from pytest import MonkeyPatch
+
+from pytest_example.pytest_example import getssh
 
 
 def test_needfiles(tmp_path: Path):
     """一時ディレクトリを作成する組み込みfixtureのtmp_pathの例
-    
+
     Args:
-        tmp_path (Path): 組み込みfixture 
+        tmp_path (Path): 組み込みfixture
     """
     print(tmp_path)
     assert isinstance(tmp_path, Path)
